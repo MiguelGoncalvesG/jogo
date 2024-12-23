8858ff239dfd5c086a6563ad63f121dcd9075665
MiguelGoncalvesG <141948958+MiguelGoncalvesG@users.noreply.github.com>
2024-12-23 20:07:21 -0300

    Update main.c
    tirando o int de teste

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 
 int main(void)
 {
-    int j;
     // Initialization
     //--------------------------------------------------------------------------------------
     InitWindow(ScreenWidth, ScreenHeight, "INF MAN - Menu");
